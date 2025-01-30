2d997ad994b956ba892b30f63087635179ac0953
YuliiaZas <julia.kv101@gmail.com>
2025-01-30 17:47:11 -0500

    feat: add fetch for people from star wars

--- src/feature/home-page/home-page.tsx
+++ src/feature/home-page/home-page.tsx
@@ -3,9 +3,12 @@ import Search from '../../lib/search/search';
 import { LOADING_STATE } from '../../utils/loading-state.enum';
 import Spinner from '../../lib/spinner/spinner';
 import { ErrorContext, ErrorContextState } from '../../utils/error-context';
+import { SearchResult } from '../../utils/search-result.interface';
+import { People } from '../../utils/people.interface';
 
 interface HomePageState {
   searchValue: string;
+  items: People[];
   loadingState: LOADING_STATE;
 }
 
@@ -16,12 +19,15 @@ class HomePage extends Component<object, HomePageState> {
 
   state: HomePageState = {
     searchValue: this.getSeachValueFromStore(),
+    items: [],
     loadingState: LOADING_STATE.PRESTINE,
   };
 
-  // componentDidMount() {
-  //   this.startSearch();
-  // }
+  url = 'https://swapi.dev/api/people?search=';
+
+  componentDidMount() {
+    this.startSearch();
+  }
 
   getSeachValueFromStore(): string {
     return window.localStorage.getItem(this.searchItemKey) ?? '';
@@ -39,7 +45,23 @@ class HomePage extends Component<object, HomePageState> {
   startSearch = (value?: string) => {
     this.setState({ loadingState: LOADING_STATE.LOADING });
     console.log('startSearch', this.state.searchValue, value);
+    this.getItems(value || this.state.searchValue);
   };
+
+  getItems = (value: string) => {
+    fetch(`${this.url}${value}`)
+      .then((response) => response.json())
+      .then((data: SearchResult<People>) => this.updateItems(data))
+      .catch(() => this.showDataError());
+  };
+
+  updateItems(data: SearchResult<People>) {
+    this.setState({ loadingState: LOADING_STATE.LOADED, items: data.results });
+  }
+
+  showDataError() {
+    this.setState({ loadingState: LOADING_STATE.FAILURE, items: [] });
+  }
 
   showPageError = () => {
     (this.context as ErrorContextState).updateShowError(true);
@@ -59,7 +81,7 @@ class HomePage extends Component<object, HomePageState> {
           <Search
             initialSearchValue={this.state.searchValue}
             updateSearchValue={this.updateSearchValue}
-            placeholder="Input Pokemon Name"
+            placeholder="Input Name from Star Wars"
           />
         </section>
         <section>
