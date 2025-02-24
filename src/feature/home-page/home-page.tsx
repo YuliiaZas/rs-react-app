03af4abd63a8070425067de8a96573fb22b5e2e1
YuliiaZas <julia.kv101@gmail.com>
2025-02-24 11:11:45 -0500

    fix: make styles mobile-friendly

--- src/feature/home-page/home-page.tsx
+++ src/feature/home-page/home-page.tsx
@@ -64,7 +64,7 @@ export const HomePage: FC = () => {
     <>
       <div className="home-wrapper">
         <main className="home-main" onClick={handleGlobalHomePageClick}>
-          <section className="home-seach">
+          <section className="home-search">
             <HomePageSearch />
           </section>
           <section className="home-content">
