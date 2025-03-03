1dade40e4603e7d0a90bb0d2fc68bf976f6fc294
YuliiaZas <julia.kv101@gmail.com>
2025-03-03 15:55:08 -0500

    Revert "refactor: services and loaders were removed"
    This reverts commit b15635fe192d703bf79ab87507546ba7f3861fc0.

---
+++ src/services/home-page.service.ts
@@ -0,0 +1,22 @@
+import { CurrentSearchParams } from '@hooks';
+import { People, peopleUnknown, PeopleUnknown, SearchResult } from '@utils';
+
+class PeopleService {
+  baseUrl = '/api/people';
+
+  async getItems(
+    paramsValue: CurrentSearchParams
+  ): Promise<SearchResult<People>> {
+    const params = new URLSearchParams(paramsValue);
+    const response = await fetch(`${this.baseUrl}?${params}`);
+    return await response.json();
+  }
+
+  async getItem(value?: string): Promise<People | PeopleUnknown> {
+    if (!value) return peopleUnknown;
+    const response = await fetch(`${this.baseUrl}/${value}`);
+    return await response.json();
+  }
+}
+
+export const peopleService = new PeopleService();
